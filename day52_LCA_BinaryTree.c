2fa33041bcc4075a32f89b1a7312a8ca48247e9b
mishraaareen <mishra.aareen81@gmail.com>
2026-03-24 18:52:02 +0530

    Initial Commit

---
+++ day52_LCA_BinaryTree.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* leftLCA = findLCA(root->left, n1, n2);
+    struct Node* rightLCA = findLCA(root->right, n1, n2);
+
+    if (leftLCA && rightLCA)
+        return root;
+
+    return (leftLCA != NULL) ? leftLCA : rightLCA;
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    int n1 = 4, n2 = 5;
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("LCA of %d and %d is %d\n", n1, n2, lca->data);
+    else
+        printf("LCA not found\n");
+
+    return 0;
+}
