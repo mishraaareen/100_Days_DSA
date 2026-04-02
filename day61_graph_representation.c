d3bf3c015494335b89b4f374a37a2d9d090bcf1f
mishraaareen <mishra.aareen81@gmail.com>
2026-04-02 22:35:57 +0530

    Initial Commit

---
+++ day61_graph_representation.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int main() {
+    int n, m;
+    int adj[MAX][MAX] = {0};
+    int u, v;
+    int isDirected;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    printf("Enter 1 for Directed graph, 0 for Undirected graph: ");
+    scanf("%d", &isDirected);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        if (!isDirected) {
+            adj[v][u] = 1;
+        }
+    }
+
+    printf("\nAdjacency Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
