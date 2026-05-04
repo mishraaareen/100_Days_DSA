63ed331f38518eec9b6337671b11b91582aaa514
Aareen Mishra <mishra.aareen81@gmail.com>
2026-05-04 19:10:03 +0530

    Initial Commit

--- day92_quick_sort.c
+++ day92_quick_sort.c
@@ -18,7 +18,6 @@ int partition(int arr[], int low, int high) {
     swap(&arr[i + 1], &arr[high]);
     return i + 1;
 }
-
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
